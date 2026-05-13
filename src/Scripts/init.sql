a6e2fa5def3f453298e844e56b5904fad0aca927
Quentin Chassel <quentin.chassel@u-bourgogne.fr>
2026-05-13 11:54:19 +0200

    Add attempt for sql init

--- src/Scripts/init.sql
+++ src/Scripts/init.sql
@@ -1,92 +1,83 @@
-/**
- * Author:  qu0262ch
- * Created: 30 avr. 2026
- */
 
-create table "years" (
-"rowid" integer not null primary key,
-"name" varchar(100) not null unique
+create table "speaker_default_management" (
+"speakerid" integer not null, -- => user(id)
+"internal" integer not null default 1, -- bool
+"manager" text not null,
+primary key("speakerid"),
+constraint "fk_speakerdefmgm_speakerid"
+foreign key ("speakerid")
+references "users"("id")
 );
 
-create table "sections" (
-"rowid" integer not null primary key,
-"name" varchar(100) not null unique
+create table "teaching_module_supervisors" (
+"teaching_module" integer not null,
+"session" integer not null,
+"supervisor" int default null,
+"internal" integer not null default 1,
+"manager" text not null,
+primary key("teaching_module", "session"),
+constraint "fk_teachingmodsup_teachingmodule"
+foreign key ("teaching_module")
+references "teaching_modules"("id"),
+constraint "fk_teachingmodsup_session"
+foreign key ("session")
+references "sessions"("id"),
+constraint "fk_teachingmodsup_supervisor"
+foreign key ("supervisor")
+references "users"("id")
 );
 
-create table "semesters" (
-"rowid" integer not null primary key,
-"name" varchar(100) not null unique
-);
-
-create table "proms" (
-"rowid" integer not null primary key,
-"yearid" integer not null,
-"sectionid" integer not null,
-"semesterid" integer not null,
-constraint "uk_proms_year_section_semester"
-unique("yearid", "sectionid", "semesterid")
-);
-
-create table "speakers" (
-"rowid" integer not null primary key,
-"name" varchar(200) not null,
-"mail" varchar(200) default null,
-"tel" varchar(50) default null
-);
-
-create table "speakermanagement" (
-"speakerid" integer not null,
-"yearid" integer not null,
-"internal"
-);
-
-create table "uestpl" (
-"rowid" integer not null primary key,
-"name" varchar(200) not null,
-"ects" integer not null default 0
-);
-
-create table "modulestpl" (
-"rowid" integer not null primary key,
-"name" varchar(200) not null,
-"code" varchar(20) default null,
-"ueid" integer not null,
-"coeff" decimal not null,
-"manager" integer default null
-);
-
-create table "repartstpl" (
-"module" integer not null,
-"mode" varchar(5) not null,
-"nb" double not null default 0,
-primary key("module", "mode")
-);
-
-create table "ues" (
-"ue" integer not null primary key,
-"name" varchar(200) not null,
-"ects" integer not null default 0
-);
-
-create table "modulestpl" (
-"module" integer not null primary key,
-"name" varchar(200) not null,
-"code" varchar(20) default null,
-"coeff" decimal not null,
-"manager" integer default null
+create table "default_reparts" (
+"teaching_module" integer not null,
+"mode" varchar(5) not null, -- enum CM,TD,TP,CI,Projet
+"nb" double not null default 0, -- seances
+"timeby" double not null, -- per seance (min)
+"groups" integer not null default 1, -- nb groups
+primary key("teaching_module", "mode"),
+primary key("teaching_module", "session"),
+constraint "fk_defreparts_teachingmodule"
+foreign key ("teaching_module")
+references "teaching_modules"("id")
 );
 
 create table "reparts" (
-"module" integer not null,
+"teaching_module" integer not null,
+"session" integer not null,
 "mode" varchar(5) not null,
 "nb" double not null default 0,
-primary key("module", "mode")
+"timeby" double not null,
+"groups" integer not null default 1,
+primary key("teaching_module", "session", "mode"),
+constraint "fk_reparts_teachingmodule"
+foreign key ("teaching_module")
+references "teaching_modules"("id"),
+constraint "fk_reparts_session"
+foreign key ("session")
+references "sessions"("id")
 );
 
 create table "speaks" (
-"module" integer not null,
+"teaching_module" integer not null,
+"session" integer not null,
 "mode" varchar(5) not null,
 "speaker" int default null,
-"nb" double not null default 0,
-primary key("module", "mode", "speaker")
+"hours" double not null default 0,
+"internal" integer not null default 1,
+"manager" text not null,
+primary key("module", "mode", "speaker"),
+constraint "fk_speaks_teachingmodule"
+foreign key ("teaching_module")
+references "teaching_modules"("id"),
+constraint "fk_speaks_session"
+foreign key ("session")
+references "sessions"("id"),
+constraint "fk_speaks_speaker"
+foreign key ("speaker")
+references "users"("id")
 );
+
+-- @TODO : créer procédure recopie intervenants Y+1 = Y
+
+
+
+
